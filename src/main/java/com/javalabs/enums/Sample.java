64eed1fa80a1e6c67109ceb015de1ca5b46fedda
Omkar Shetkar <omkar.shetkar@outlook.com>
2023-01-19 07:51:25 +0530

    Java Enums

--- src/main/java/com/javalabs/enums/Sample.java
+++ src/main/java/com/javalabs/enums/Sample.java
@@ -1,71 +1,80 @@
 package com.javalabs.enums;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Sample {
 
     public static void main(String[] args) {
-//        printSeason(SeasonConstants.FALL);
-//        printSeason("Foggy");
 
-//        printSeason(Season.Foggy);
+//        Set<Season> set = EnumSet.of(Season.WINTER, Season.FALL, Season.SPRING);
+//
+//        set.forEach(s -> System.out.println(s.name() + " "+s.ordinal()));
 
-//        Arrays.stream(Season.values()).forEach(s -> System.out.println(s.name() + " "+s.ordinal()));
+        Map<Season, String> map = new EnumMap<Season, String>(Season.class);
+        map.put(Season.FALL, "Its fall!");
+        map.put(Season.SUMMER, "Its summer!");
+        map.put(Season.SPRING, "Its spring!");
 
-        System.out.println(Season.SPRING.hashCode());
-        System.out.println(Season.valueOf("SPRING").hashCode());
-
+        map.entrySet().forEach(e -> System.out.println(e.getKey().name()+ " "+e.getKey().ordinal()));
     }
 
-    public static void printSeason(String season) {
-        switch (season) {
-            case SeasonConstants.WINTER:
-                System.out.println("Its Winter!");
-                break;
-            case SeasonConstants.SPRING:
-                System.out.println("Its Spring!");
-                break;
-            case SeasonConstants.SUMMER:
-                System.out.println("Its Summer!");
-                break;
-            case SeasonConstants.FALL:
-                System.out.println("Its Fall!");
-                break;
-            default:
-                System.out.println("Unknown season!");
-                break;
-        }
 
-    }
-
-    public static void printSeason(Season season) {
+    private static void printSeason(Season season) {
         switch (season) {
             case WINTER:
-                System.out.println("Its Winter!");
-                break;
-            case SPRING:
-                System.out.println("Its Spring!");
-                break;
-            case SUMMER:
-                System.out.println("Its Summer!");
+                System.out.println("It's winter!");
                 break;
             case FALL:
-                System.out.println("Its Fall!");
-                break;
-            default:
-                System.out.println("Unknown season!");
+                System.out.println("It's fall!");
                 break;
         }
     }
-}
 
-enum Season {
-    WINTER, SPRING, SUMMER, FALL
-}
 
-class SeasonConstants {
-    public static final String WINTER = "winter";
-    public static final String SPRING = "spring";
-    public static final String SUMMER = "summer";
-    public static final String FALL = "fall";
+    enum Season {
+        WINTER("winter") {
+            @Override
+            void printDescription() {
+                System.out.println(this.description);
+            }
+
+            @Override
+            public void moreDescription() {
+                System.out.println("It's very cold here!");
+            }
+        },
+        SPRING("spring"){
+            @Override
+            void printDescription() {
+                System.out.println(this.description);
+            }
+        },
+        SUMMER("summer"){
+            @Override
+            void printDescription() {
+                System.out.println(this.description);
+            }
+        },
+        FALL("fall"){
+            @Override
+            void printDescription() {
+                System.out.println(this.description);
+            }
+        };
+
+         final String description;
+
+        Season(String description) {
+            this.description = description;
+        }
+
+        abstract void printDescription();
+
+        public void moreDescription() {
+            System.out.println("More of "+description);
+        }
+
+    }
+
+
 }
